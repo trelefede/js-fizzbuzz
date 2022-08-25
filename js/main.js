77d70dd753c38a2206c1489a2edc7f4a06d4a520
TreleaniFederico <treleani.federico@gmail.com>
2022-08-25 16:16:43 +0200

    add for cicle add if to divisible numbers

--- js/main.js
+++ js/main.js
@@ -8,8 +8,28 @@
 
 
 
-
-
 console.log('js ok');
 
 // ciclo for per scrivere numeri da 1 a 100
+for (let i = 1; i < 101; i++) {
+
+
+    // codizione per i divisibili per 3 e 5 
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log('fizzbuzz')
+    }
+    //condizione per i divisibili per 3
+    else if (i % 3 === 0) {
+        console.log('fizz')
+    }
+    //condizione per i divisibili per 5
+    else if (i % 5 === 0) {
+        console.log('buzz')
+    }
+
+    // nessuna condizione 
+    else {
+        console.log(i)
+    }
+
+}
