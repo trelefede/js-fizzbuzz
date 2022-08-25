3248de9cd7dd6b97ea4bee3f7e55bc52ebad769a
TreleaniFederico <treleani.federico@gmail.com>
2022-08-25 14:54:11 +0200

    add html and js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100, 
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. 
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+// BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. 
+// Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+
+
+
+
+
+console.log('js ok');
+
+// ciclo for per scrivere numeri da 1 a 100
